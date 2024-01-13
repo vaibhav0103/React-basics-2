ddee59d6ec2459f01c9ea3d46b8c920e529a63c6
Vaibhav <vaibhavsharma1393@gmail.com>
2024-01-13 16:32:27 +0530

    react inint

---
+++ App.js
@@ -0,0 +1,14 @@
+const parent = React.createElement(
+  "div",
+  { id: "heading" },
+  React.createElement("h1", { id: "child" }, [
+    React.createElement("h2", { id: "heading" }, "Hello world! I'm Sibling 1"),
+    React.createElement("h2", { id: "heading" }, "Hello world! I'm Sibling 2"),
+  ])
+);
+
+// const heading = React.createElement("h1", { id: "heading" }, "Hello world!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+root.render(parent)
